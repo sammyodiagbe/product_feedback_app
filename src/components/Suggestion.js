ad5eb7fec6bb1ec5389268ee3d3e903667b6069a
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-12 11:39:00 -0600

    working on suggestion page

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,5 +1,25 @@
+import commentIcon from "../assets/shared/icon-comments.svg";
+import arrowUp from "../assets/shared/icon-arrow-up.svg";
+
 const Suggestion = ({ data }) => {
-  return <div className="pf-suggestion"></div>;
+  const { category, comments, title, description, upvotes } = data;
+  return (
+    <div className="pf-suggestion">
+      <div className="pf-details-container">
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <span className="pf-category">{category}</span>
+      </div>
+      <button className="pf-upvote-button">
+        <img className="pf-arrow-button" src={arrowUp} />
+        <span className="pf-upvote">{upvotes}</span>
+      </button>
+      <span className="pf-comments">
+        <img src={commentIcon} className="pf-comment-icon" />
+        {comments.length}
+      </span>
+    </div>
+  );
 };
 
 export default Suggestion;
