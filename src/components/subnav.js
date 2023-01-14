3293ad5ecebe1a9319a6e3b1a35e28ca21bdc8e4
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-14 12:33:45 -0600

    working on structure

--- src/components/subnav.js
+++ src/components/subnav.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import dropDown from "../assets/shared/icon-arrow-down.svg";
 import dropUp from "../assets/shared/icon-arrow-up.svg";
+import { dataContext } from "../context/dataContext";
+import checkMark from "../assets/shared/icon-check.svg";
 
 function customDropDown(value, open, toggleFilter) {
   return (
@@ -14,13 +16,30 @@ function customDropDown(value, open, toggleFilter) {
 }
 
 const SubNav = () => {
+  const context = useContext(dataContext);
   const [openFilter, toggleFilter] = useState(false);
   const [value, setValue] = useState("Most comments");
+  const [activeId, setActiveId] = useState(1);
 
   const toggle = () => {
     toggleFilter(!openFilter);
   };
 
+  const sortByMostUpvotes = () => {
+    setActiveId(1);
+  };
+
+  const sortByLeastUpvotes = () => {
+    setActiveId(2);
+  };
+
+  const sortByMostComments = () => {
+    setActiveId(3);
+  };
+
+  const sortByLeastComments = () => {
+    setActiveId(4);
+  };
   return (
     <div className="pf-sub-nav">
       {/* bulb logo and suggestions total */}
@@ -32,10 +51,22 @@ const SubNav = () => {
       <Link className="pf-add-feedback">+ Add Feedback</Link>
       {openFilter && (
         <div className="pf-drop-down">
-          <button className="pf-drop-down-item">Most Upvotes</button>
-          <button className="pf-drop-down-item">Least Upvotes</button>
-          <button className="pf-drop-down-item">Most Comments</button>
-          <button className="pf-drop-down-item">Least Comments</button>
+          <button className="pf-drop-down-item" onClick={sortByMostUpvotes}>
+            <span>Most Upvotes</span>
+            {activeId == 1 && <img src={checkMark} alt="check mark" />}
+          </button>
+          <button className="pf-drop-down-item" onClick={sortByLeastUpvotes}>
+            <span>Least Upvotes</span>
+            {activeId == 2 && <img src={checkMark} alt="check mark" />}
+          </button>
+          <button className="pf-drop-down-item" onClick={sortByMostComments}>
+            <span>Most Comments</span>
+            {activeId == 3 && <img src={checkMark} alt="check mark" />}
+          </button>
+          <button className="pf-drop-down-item" onClick={sortByLeastComments}>
+            <span>Least Comments</span>
+            {activeId == 4 && <img src={checkMark} alt="check mark" />}
+          </button>
         </div>
       )}
     </div>
