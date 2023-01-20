c13a1b60ef4fcf67730b2130c5d4c9c1680a0e72
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-20 15:55:24 -0600

    working on roadmap page

--- src/pages/roadmap.js
+++ src/pages/roadmap.js
@@ -4,7 +4,10 @@ import { dataContext } from "../context/dataContext";
 
 const RoadMap = () => {
   const context = useContext(dataContext);
-  const { plans, inProgress, live } = context;
+  console.log(context);
+  const { plans, inProgress, liveData } = context;
+
+  const renderplans = plans.map((plan, index) => null);
 
   const toggleClass = (event) => {
     const { target } = event;
@@ -15,10 +18,21 @@ const RoadMap = () => {
     <>
       <SubNav hideDrop={true} showBackLink={true} />
       {/* road map tracker */}
-      <div className="pf-roadmap-nav"></div>
+      <div className="pf-roadmap-nav">
+        <div className="pf-nav-top">
+          <button className="pf-switch">Planned({plans.length})</button>
+          <button className="pf-switch">
+            In-Progress({inProgress.length})
+          </button>
+          <button className="pf-switch">Live({liveData.length})</button>
+        </div>
+      </div>
       <div className="pf-content">
         {/* sliding them out shoudl be alot easier if we make the container 3 * the size of the screen then we are able to move them accordingly show the 
         current element we want to show on the screen */}
+        <div className="pf-planned-container"></div>
+        <div className="pf-inprogress-container"></div>
+        <div className="pf-live-container"></div>
       </div>
     </>
   );
