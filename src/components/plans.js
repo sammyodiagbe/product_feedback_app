6861070f73f78ab8c1a6ba8cd00c58ab3a4940b1
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-20 16:15:04 -0600

    working on the roadmap page

--- src/components/plans.js
+++ src/components/plans.js
@@ -1,5 +1,36 @@
-const Planned = () => {
-  return <></>;
+import upArrowIcon from "../assets/shared/icon-arrow-up.svg";
+import commentIcon from "../assets/shared/icon-comment.svg";
+
+const RoadmapContainer = ({ type, data }) => {
+  const { title, upvotes, category, comments, description } = data;
+
+  const classname =
+    type === "planned"
+      ? "pf-orange"
+      : type === "In Progress"
+      ? "pf-purple"
+      : "pf-blue";
+  return (
+    <div className="pf-roadmap">
+      <div className="pf-r">
+        <span className={`pf-color ${classname}`}></span>{" "}
+        <span className="pf-type"> {type}</span>
+      </div>
+      <div className="pf-content">
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <span className="category">{category}</span>
+        <div className="pf-upvote-category">
+          <button className="pf-upvote">
+            <img src={upArrowIcon} alt="upvote icon" /> {upvotes}
+          </button>
+          <button className="pf-comments">
+            <img src={commentIcon} alt="comments icon" /> {comments.length}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Planned;
+export default RoadmapContainer;
