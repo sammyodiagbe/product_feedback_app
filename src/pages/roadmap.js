188941f9d304220bec34714541ee87caf5a49b1a
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-20 11:50:52 -0600

    working on showing the data accordingly

--- src/pages/roadmap.js
+++ src/pages/roadmap.js
@@ -6,7 +6,10 @@ const RoadMap = () => {
       <SubNav hideDrop={true} showBackLink={true} />
       {/* road map tracker */}
       <div className="pf-roadmap-nav"></div>
-      <div className="pf-content"></div>
+      <div className="pf-content">
+        {/* sliding them out shoudl be alot easier if we make the container 3 * the size of the screen then we are able to move them accordingly show the 
+        current element we want to show on the screen */}
+      </div>
     </>
   );
 };
