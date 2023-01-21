e0a82d43b370951c4e5cf60fd56c46acabf4401a
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-21 14:15:41 -0600

    working on styling of roadmap page

--- src/pages/roadmap.js
+++ src/pages/roadmap.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import RoadmapContainer from "../components/roadmapContainer";
 import SubNav from "../components/subnav";
 import { dataContext } from "../context/dataContext";
@@ -7,6 +7,8 @@ const RoadMap = () => {
   const context = useContext(dataContext);
   console.log(context);
   const { plans, inProgress, liveData } = context;
+  const [activeState, setActiveState] = useState("Planned");
+  const [movementClass, setMovementClass] = useState("Planned");
 
   const renderplans = plans.map((plan, index) => {
     return <RoadmapContainer data={plan} type="Planned" key={index} />;
@@ -22,10 +24,25 @@ const RoadMap = () => {
     return <RoadmapContainer data={live} type="Live" key={index} />;
   });
 
-  const toggleClass = (event) => {
+  const changeActiveViewState = (event) => {
     const { target } = event;
-    const { id } = target.dataset;
+    const { name } = target.dataset;
+    setActiveState(name);
+    setMovementClass(name);
   };
+
+  const messages = {
+    "In-Progress": "Features currently being developed",
+    Live: "Released Features",
+    Planned: "Ideas prioritized for research.",
+  };
+
+  const length =
+    activeState === "Planned"
+      ? renderplans.length
+      : activeState === "In-Progress"
+      ? renderInProgress.length
+      : renderLive.length;
 
   return (
     <>
@@ -33,14 +50,34 @@ const RoadMap = () => {
       {/* road map tracker */}
       <div className="pf-roadmap-nav">
         <div className="pf-nav-top">
-          <button className="pf-switch">Planned ({plans.length})</button>
-          <button className="pf-switch">
+          <button
+            className="pf-switch"
+            data-name="Planned"
+            onClick={changeActiveViewState}
+          >
+            Planned ({plans.length})
+          </button>
+          <button
+            className="pf-switch"
+            data-name="In-Progress"
+            onClick={changeActiveViewState}
+          >
             In-Progress ({inProgress.length})
           </button>
-          <button className="pf-switch">Live ({liveData.length})</button>
+          <button
+            className="pf-switch"
+            data-name="Live"
+            onClick={changeActiveViewState}
+          >
+            Live ({liveData.length})
+          </button>
         </div>
       </div>
-      <div className="pf-content">
+      <div className="pf-state-details">
+        <h1>{`${activeState} (${length})`}</h1>
+        <p>{`${messages[activeState]}`}</p>
+      </div>
+      <div className={`pf-content ${movementClass}`}>
         {/* sliding them out shoudl be alot easier if we make the container 3 * the size of the screen then we are able to move them accordingly show the 
         current element we want to show on the screen */}
         <div className="pf-r-container">{renderplans}</div>
