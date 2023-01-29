bff4088b05fa2a5a12bf1c90deb6f0f62ea655d5
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-28 20:27:47 -0600

    working on comments ui

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,17 +1,33 @@
 const Comment = ({ data }) => {
   const { id, user, content, replies } = data;
+
   const { username, name, image } = user;
   const url = "." + image;
+
+  const repliesStructure = replies
+    ? replies.length
+      ? replies.map((reply, index) => {
+          return <Comment data={reply} key={index} />;
+        })
+      : null
+    : null;
   return (
-    <div className="pf-comment">
-      {/* <div className="pf-comment-head"> */}
-      <img src={url} alt="user profile picture" className="user-image" />
-      <div className="pf-name">
-        <h3>{name}</h3>
-        <p>@{username}</p>
+    <div className="pf-comment-container">
+      <div className="pf-comment">
+        {/* <div className="pf-comment-head"> */}
+        <img src={url} alt="user profile picture" className="user-image" />
+        <div className="pf-name">
+          <h3>{name}</h3>
+          <p>@{username}</p>
+        </div>
+        <button className="reply-toggle">reply</button>
+        <p className="content">{content}</p>
       </div>
-      <button className="reply-toggle">reply</button>
-      <p className="content">{content}</p>
+      <div className="pf-replies">
+        {repliesStructure && (
+          <div className="pf-replies">{repliesStructure}</div>
+        )}
+      </div>
     </div>
   );
 };
