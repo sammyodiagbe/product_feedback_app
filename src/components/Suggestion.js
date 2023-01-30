06099493c1b104295c3d59dde2f65c7d96b8154e
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-01-29 22:06:00 -0600

    working on functionality for app

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,24 +1,27 @@
 import commentIcon from "../assets/shared/icon-comments.svg";
 import arrowUp from "../assets/shared/icon-arrow-up.svg";
+import { Link } from "react-router-dom";
 
 const Suggestion = ({ data }) => {
-  const { category, comments, title, description, upvotes } = data;
+  const { category, comments, title, description, upvotes, id } = data;
   return (
-    <div className="pf-suggestion">
-      <button className="pf-upvote-button">
-        <img className="pf-arrow-button" src={arrowUp} />
-        <span className="pf-upvote">{upvotes}</span>
-      </button>
-      <div className="pf-details-container">
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <span className="pf-category">{category}</span>
+    <Link to={`/feedback/${id}`} style={{ color: "#555" }}>
+      <div className="pf-suggestion">
+        <button className="pf-upvote-button">
+          <img className="pf-arrow-button" src={arrowUp} />
+          <span className="pf-upvote">{upvotes}</span>
+        </button>
+        <div className="pf-details-container">
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <span className="pf-category">{category}</span>
+        </div>
+        <span className="pf-comments">
+          <img src={commentIcon} className="pf-comment-icon" />
+          {comments ? comments.length : 0}
+        </span>
       </div>
-      <span className="pf-comments">
-        <img src={commentIcon} className="pf-comment-icon" />
-        {comments ? comments.length : 0}
-      </span>
-    </div>
+    </Link>
   );
 };
 
